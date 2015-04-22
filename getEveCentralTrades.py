066b134559eed33bee311fefc27d2a12f726cf76
GallicaGD <gallica@gmail.com>
2015-04-21 21:05:47 -0500

    Changes

--- getEveCentralTrades.py
+++ getEveCentralTrades.py
@@ -20,31 +20,74 @@ import urllib.request
 import urllib.parse
 import json
 import sqlite3
+import time
 
-db_data = 'D:\\EVEData\\eveData.sqlite3'
+db_eveData = 'D:\\EVEData\\eveData.sqlite3'
+db_eveSDE = 'D:\\EVEData\\sqlite-scylla.sqlite'
+##b_eveSDE = 'D:\\EVEData\\sqlite-latest.sqlite'
 
-def storeData(dbfile, data):
-    conn = sqlite3.connect(dbfile)
-    conn.isolation_level = None
+runID = int(time.time())
 
-    cols = ( 'typeID', 'systemID', 'regionID', 'buyMax', 'buyMin', 'buyMedian',
-    'buyFivePercent', 'buyVolume', 'sellMax', 'sellMin', 'sellMedian',
-    'sellFivePercent', 'sellVolume')
+sdeconn = sqlite3.connect(db_eveSDE)
+dataconn = sqlite3.connect(db_eveData)
 
-    sql = 'insert into eveCentralData(enteredDate,'
-    sql += ','.join(cols) + ") values (datetime('now'), " + ','.join('?' for i in range(len(cols))) + ')'
+def getIDForName(name, type='system'):
+
+    if type == 'system':
+        sql = 'select solarSystemID from mapSolarSystems where solarSystemName = ?'
+    if type == 'region':
+        sql = 'select regionID from mapRegions where regionName = ?'
+    if type == 'market':
+        pass
+    if type == 'group':
+        pass
+    if type == 'item':
+        pass
+
+    id = ''
+
+    if name:
+        for row in sdeconn.execute(sql, (name,)):
+            id = row[0]
+
+    return id
+
+def getNameForID(id, type='system'):
+
+    if type == 'system':
+        sql = 'select solarSystemName from mapSolarSystems where solarSystemID = ?'
+    if type == 'region':
+        sql = 'select regionName from mapRegions where regionID = ?'
+    if type == 'item':
+        sql = 'select typeName from invTypes where typeID = ?'
+
+    name = ''
+
+    if id:
+        for row in sdeconn.execute(sql, (id,)):
+            name = row[0]
+
+    return name
+
+
+def storeData(data, table='eveCentralData'):
+
+    dataconn.isolation_level = None
 
     rows = []
     for d in data:
-        tmp = []
-        for col in cols:
-            tmp.append(d[col])
-        rows.append(tmp)
+        cols = data[0].keys()
+        tmp = d.values()
+        rows.append(list(tmp))
 
-    conn.executemany(sql, rows)
+    sql = "insert into {table}( enteredDate, {columns} ) values ( datetime('now'), {bind} )".format(
+        table = table, columns = ','.join(cols), bind =','.join('?' for i in range(len(cols)))
+    )
 
-    print('Total Rows: {0}'.format(conn.total_changes))
-    conn.close()
+    dataconn.executemany(sql, rows)
+
+    print('Total Rows: {0}'.format(dataconn.total_changes))
+
 
 def eveCentral(typeids, system=30000142, region=None):
     payload = {}
@@ -68,37 +111,40 @@ def eveCentral(typeids, system=30000142, region=None):
 
     data_store = []
     for item in prices:
-        tmp = {}
-        tmp['buyMax'] = item['buy']['max']
-        tmp['buyMin'] = item['buy']['min']
-        tmp['buyMedian'] = item['buy']['median']
-        tmp['buyFivePercent'] = item['buy']['fivePercent']
-        tmp['buyVolume'] = item['buy']['volume']
-        tmp['sellMax'] = item['sell']['max']
-        tmp['sellMin'] = item['sell']['min']
-        tmp['sellMedian'] = item['sell']['median']
-        tmp['sellFivePercent'] = item['sell']['fivePercent']
-        tmp['sellVolume'] = item['sell']['volume']
-        tmp['typeID'] = item['all']['forQuery']['types'][0]
+        tmp = {
+            'buyMax': item['buy']['max'],
+            'buyMin': item['buy']['min'],
+            'buyMedian': item['buy']['median'],
+            'buyFivePercent': item['buy']['fivePercent'],
+            'buyVolume': item['buy']['volume'],
+            'sellMax': item['sell']['max'],
+            'sellMin': item['sell']['min'],
+            'sellMedian': item['sell']['median'],
+            'sellFivePercent': item['sell']['fivePercent'],
+            'sellVolume': item['sell']['volume'],
+            'typeID': item['all']['forQuery']['types'][0],
+            'typeName': getNameForID(item['all']['forQuery']['types'][0], 'item'),
+            'systemID': '',
+            'systemName': '',
+            'regionID': '',
+            'regionName': '',
+            'runID': runID
+        }
 
-        if system:
+        if item['all']['forQuery']['systems']:
             tmp['systemID'] = item['all']['forQuery']['systems'][0]
-        else:
-            tmp['systemID'] = ''
-        if region:
+            tmp['systemName'] = getNameForID(item['all']['forQuery']['systems'][0], 'system')
+        if item['all']['forQuery']['regions']:
             tmp['regionID'] = item['all']['forQuery']['regions'][0]
-        else:
-            tmp['regionID'] = ''
-
+            tmp['regionName'] = getNameForID(item['all']['forQuery']['regions'][0], 'region')
 
         data_store.append(tmp)
 
-
-    storeData(db_data, data_store)
+    storeData(data_store)
 
 def main():
     TYPEIDS = [ 34,35,36,37,38,39,40,11399,  # Minerals
-		19,20121,22,1223,1224,1225,1226,1227,1228,1229,1230,1231,1232,
+		19,20,21,22,1223,1224,1225,1226,1227,1228,1229,1230,1231,1232,
 		11396,17425,17426,17428,17429,17432,17433,17436,17437,17440,17441,
 		17444,17445,17448,17449,17452,17453,17455,17456,17459,17460,17463,
 		17464,17466,17467,17470,17471,17865,17866,17867,17868,17869,17870,   # ORES
@@ -107,15 +153,31 @@ def main():
 		3689,44,9832,9848,3683  # PI for fuel blocks
 	]
 
+    ## 4 main hubs and the extra for Metro
     SYSTEMIDS = [
         30002187,	## Amarr
         30002659,	## Dodixie
         30000142,	## Jita
         30002510,	## Rens
+        30002053,   ## Hek
     ]
+    systemnames = ('Amarr', 'Dodixie','Jita', 'Rens', 'Hek')
+    regionNames = ( 'Heimatar', 'Metropolis', 'Molden Heath')
 
-    for systemID in SYSTEMIDS:
-        eveCentral(TYPEIDS, system=systemID)
+    for name in systemnames:
+        systemID = getIDForName(name, 'system')
+        print("System: %s | %s" % (name, systemID))
+        eveCentral(TYPEIDS,system=systemID)
+        time.sleep(1)
+
+    for name in regionNames:
+        regionID = getIDForName(name, 'region')
+        print("Region: %s | %s" % (name, regionID))
+        eveCentral(TYPEIDS,system=None,region=regionID)
+        time.sleep(1)
+
+##    for systemID in SYSTEMIDS:
+##        eveCentral(TYPEIDS, system=systemID)
 
 
 if __name__ == '__main__':
